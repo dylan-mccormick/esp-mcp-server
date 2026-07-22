94c61b3c92441abfb3e20448e5265a36f3b231dc
Dylan McCormick <mccormick.dy@northeastern.edu>
2026-07-22 16:29:21 -0400

    connect to wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,18 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <env.h>
+
+void setup() {
+    Serial.begin(115200);
+
+    // Connect to WiFi
+    Serial.print("Connecting to WiFi");
+    WiFi.begin(WIFI_SSID, WIFI_PWD);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(100);
+        Serial.print(".");
+    }
+    Serial.println("done!");
+}
+
+void loop() {}
