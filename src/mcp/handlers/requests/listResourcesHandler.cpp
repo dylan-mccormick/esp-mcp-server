37b73923eabae8f08b4448f411dcc56b1ccb57b4
Dylan McCormick <mccormick.dy@northeastern.edu>
2026-08-10 08:58:16 -0400

    list/read resource handlers

---
+++ src/mcp/handlers/requests/listResourcesHandler.cpp
@@ -0,0 +1,26 @@
+// listResourcesHandler.cpp
+// Contains a handler for resources/list call
+
+#include "mcp/registries/mcpResourceRegistry.h"
+#include "mcp/registries/mcpRequestRegistry.h"
+
+McpRequestHandlerResult listResourcesHandler(const JsonObjectConst args, JsonVariant result) {
+    JsonArray resourcesArr = result["resources"].to<JsonArray>();
+    for (const auto& [_, details] : McpResourceRegistry::handlers()) {
+        JsonObject data = resourcesArr.add<JsonObject>();
+
+        // write resource data
+        data["uri"] = details.uri;
+        data["name"] = details.name;
+        data["description"] = details.description;
+        data["mimeType"] = details.mimeType;
+    }
+
+    // cache data
+    result["ttlMs"] = 300000;
+    result["cacheScope"] = "public";
+
+    return McpRequestHandlerResult::success();
+}
+
+MCP_REQUEST_HANDLER("resources/list", listResourcesHandler);
---
+++ src/mcp/handlers/requests/readResourceHandler.cpp
@@ -0,0 +1,26 @@
+// readResourceHandler.cpp
+// Contains a handler for resources/read call
+
+#include <ArduinoJson.h>
+#include "mcp/handlers/tools/toolUtils.h"
+#include "mcp/registries/mcpResourceRegistry.h"
+#include "mcp/registries/mcpRequestRegistry.h"
+
+McpRequestHandlerResult readResourceHandler(const JsonObjectConst params, JsonVariant result) {
+    String uri;
+    if (!requireArg(params, "uri", uri)) {
+        return McpRequestHandlerResult::error(-32602, "uri must be provided as a string");
+    }
+
+    auto& handlers = McpResourceRegistry::handlers();
+    auto target = handlers.find(uri);
+    if (target == handlers.end()) {
+        return McpRequestHandlerResult::error(-32602, String("Resource not found: ") + uri);
+    }
+
+    // execute handler
+    target->second.handler(result);
+    return McpRequestHandlerResult::success();
+}
+
+MCP_REQUEST_HANDLER("resources/read", readResourceHandler);
