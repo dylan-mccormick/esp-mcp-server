414942ca5ffd9d2353c558021a1298cb2ba51951
Dylan McCormick <mccormick.dy@northeastern.edu>
2026-08-19 08:53:19 -0400

    update task manager to just call each task's own initialization method

--- src/tasks/taskManager.cpp
+++ src/tasks/taskManager.cpp
@@ -1,8 +1,6 @@
 // taskManager.cpp
 // Contains logic to manage all FreeRTOS task definitions
 
-#include <Arduino.h>
-
 #include "refreshFrontendTask.h"
 #include "pinWriteSequenceScheduler.h"
 
---
+++ src/tasks/taskManager.h
@@ -0,0 +1,6 @@
+// taskManager.h
+// Contains a definition to start all background tasks
+
+#pragma once
+
+void startAllBackgroundTasks();
