97a55d53f938a48cb93597e7da611f57dd8e2b3f
Dylan McCormick <mccormick.dy@northeastern.edu>
2026-08-23 13:31:20 -0400

    wifi timeout

--- src/main.cpp
+++ src/main.cpp
@@ -9,19 +9,28 @@
 #include "tasks/taskManager.h"
 #include "web/router.h"
 
+#ifndef WIFI_CONNECT_TIMEOUT_MS
+#define WIFI_CONNECT_TIMEOUT_MS 30000
+#endif
+
 void setup() {
     Serial.begin(115200);
 
     // Connect to WiFi
     Serial.print("Connecting to WiFi");
     WiFi.begin(WIFI_SSID, WIFI_PWD);
-    while (WiFi.status() != WL_CONNECTED) {
+    const unsigned long wifiStart = millis();
+    while (WiFi.status() != WL_CONNECTED && millis() - wifiStart < WIFI_CONNECT_TIMEOUT_MS) {
         delay(100);
         Serial.print(".");
     }
-    Serial.println("done!");
-    Serial.print("Local IP address: ");
-    Serial.println(WiFi.localIP());
+    if (WiFi.status() == WL_CONNECTED) {
+        Serial.println("done!");
+        Serial.print("Local IP address: ");
+        Serial.println(WiFi.localIP());
+    } else {
+        Serial.println("WiFi connection timed out.");
+    }
 
     // Static file handling init
     if (!LittleFS.begin(true)) {
